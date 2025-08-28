fd5ecff3b5ac47f1f6bff0a495f8a205adc84c0e
Sk Md Rizwan <skmdrizwan2003@gmail.com>
2025-08-28 21:46:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (75.00%) - LeetSync

---
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.go
@@ -0,0 +1,26 @@
+func sortMatrix(grid [][]int) [][]int {
+  n := len(grid)
+  for i := range n {
+    var s []int
+    for j := range n-i {
+      s = append(s, grid[i+j][j])
+    }
+    sort.Slice(s, func(x, y int) bool { return s[x] > s[y] })
+    for j := range n-i {
+      grid[i+j][j] = s[j]
+    }
+  }
+
+  for j:=1; j<n; j++ {
+    var s []int
+    for i := range n-j {
+      s = append(s, grid[i][i+j])
+    }
+    sort.Ints(s)
+    for i := range n-j {
+      grid[i][i+j] = s[i]
+    }
+  }
+
+  return grid
+}
