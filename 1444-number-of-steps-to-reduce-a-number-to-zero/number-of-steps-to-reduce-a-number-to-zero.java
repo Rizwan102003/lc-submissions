689083600e99050c89de7b993e3e6617a9ecc143
Sk Md Rizwan <skmdrizwan2003@gmail.com>
2025-09-24 17:46:31 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (5.97%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,14 +1,8 @@
-public class Solution {
-    public int numberOfSteps(int n) {
-        if (n == 0) return 0;
-        int ones = Integer.bitCount(n);
-        int highestBitIndex = 31 - Integer.numberOfLeadingZeros(n);
-        return ones + highestBitIndex;
-    }
-    public int numberOfStepsLong(long n) {
-        if (n == 0L) return 0;
-        int ones = Long.bitCount(n);
-        int highestBitIndex = 63 - Long.numberOfLeadingZeros(n);
+class Solution {
+    public int numberOfSteps(int num) {
+        if (num == 0) return 0;
+        int ones = Integer.bitCount(num);
+        int highestBitIndex = 31 - Integer.numberOfLeadingZeros(num);
         return ones + highestBitIndex;
     }
 }
