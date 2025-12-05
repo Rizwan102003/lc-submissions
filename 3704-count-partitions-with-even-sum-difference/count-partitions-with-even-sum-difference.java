358159af8607edd8cbc37fc92baf9a913a2d1c5f
Sk Md Rizwan <skmdrizwan2003@gmail.com>
2025-12-05 21:45:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (24.46%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
@@ -2,12 +2,9 @@ class Solution {
     public int countPartitions(int[] nums) {
         int count=0;
         int max=0;
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<nums.length;i++)
             max+=nums[i];
-        }
-        for(int i=0;i<nums.length;i++){
-            if(max%2!=0) return 0;
-        }
+        if(max%2!=0) return 0;
         return nums.length-1;
     }
 }
